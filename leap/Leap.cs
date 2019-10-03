976e9cfbd41c8c175f6ad7914b1d33ac5a1f3d06
Quoc Anh Trinh <trinhquocanh94@gmail.com>
2019-10-03 20:47:07 +0200

    Solve Leap

--- leap/Leap.cs
+++ leap/Leap.cs
@@ -2,8 +2,5 @@ using System;
 
 public static class Leap
 {
-    public static bool IsLeapYear(int year)
-    {
-        throw new NotImplementedException("You need to implement this function.");
-    }
+    public static bool IsLeapYear(int year) => year % 4 == 0 & year % 100 != 0 ^ year % 400 == 0;
 }

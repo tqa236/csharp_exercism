208f93a78e6061e2861f2e8b99c0cda0f1ce310e
Quoc Anh Trinh <trinhquocanh94@gmail.com>
2019-10-05 09:23:29 +0200

    Solve Gigasecond and add new exercises

--- gigasecond/Gigasecond.cs
+++ gigasecond/Gigasecond.cs
@@ -2,8 +2,5 @@ using System;
 
 public static class Gigasecond
 {
-    public static DateTime Add(DateTime moment)
-    {
-        throw new NotImplementedException("You need to implement this function.");
-    }
+    public static DateTime Add(DateTime moment) => moment.AddSeconds(1e9);
 }

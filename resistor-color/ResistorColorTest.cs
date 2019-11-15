c533c5db66579421deb022631fa994d103a3349d
Quoc Anh Trinh <trinhquocanh94@gmail.com>
2019-11-15 20:39:33 +0100

    Try Codecov

--- resistor-color/ResistorColorTest.cs
+++ resistor-color/ResistorColorTest.cs
@@ -2,29 +2,24 @@
 
 using Xunit;
 
-public class ResistorColorTest
-{
+public class ResistorColorTest {
     [Fact]
-    public void Black()
-    {
-        Assert.Equal(0, ResistorColor.ColorCode("black"));
+    public void Black () {
+        Assert.Equal (0, ResistorColor.ColorCode ("black"));
     }
 
     [Fact]
-    public void White()
-    {
-        Assert.Equal(9, ResistorColor.ColorCode("white"));
+    public void White () {
+        Assert.Equal (9, ResistorColor.ColorCode ("white"));
     }
 
     [Fact]
-    public void Orange()
-    {
-        Assert.Equal(3, ResistorColor.ColorCode("orange"));
+    public void Orange () {
+        Assert.Equal (3, ResistorColor.ColorCode ("orange"));
     }
 
     [Fact]
-    public void Colors()
-    {
-        Assert.Equal(new[] { "black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white" }, ResistorColor.GetColors());
+    public void Colors () {
+        Assert.Equal (new [] { "black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white" }, ResistorColor.Colors ());
     }
 }

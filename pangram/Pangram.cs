52840bfc1b16ee6bcf6030ede24f118b1d5ad1c0
Quoc Anh Trinh <trinhquocanh94@gmail.com>
2019-11-16 13:55:57 +0100

    Solve Pangram

--- pangram/Pangram.cs
+++ pangram/Pangram.cs
@@ -1,9 +1,13 @@
 ﻿using System;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
 
 public static class Pangram
 {
     public static bool IsPangram(string input)
     {
-        throw new NotImplementedException();
+        string clean_input = Regex.Replace(input.ToLower(), "[^a-z]", "");
+        var uniqueChars = new HashSet<Char>(clean_input.ToCharArray());
+        return uniqueChars.Count == 26;
     }
 }

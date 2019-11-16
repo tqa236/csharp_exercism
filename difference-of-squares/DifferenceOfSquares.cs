681bb75756288bad8ba952b1851b792117d3cbf6
Quoc Anh Trinh <trinhquocanh94@gmail.com>
2019-11-16 14:28:01 +0100

    Solve difference of square

--- difference-of-squares/DifferenceOfSquares.cs
+++ difference-of-squares/DifferenceOfSquares.cs
@@ -1,19 +1,10 @@
 ﻿using System;
-
+using System.Linq;
 public static class DifferenceOfSquares
 {
-    public static int CalculateSquareOfSum(int max)
-    {
-        throw new NotImplementedException("You need to implement this function.");
-    }
+    public static int CalculateSquareOfSum(int max) => (int)Math.Pow(Enumerable.Range(1, max).Sum(), 2);
 
-    public static int CalculateSumOfSquares(int max)
-    {
-        throw new NotImplementedException("You need to implement this function.");
-    }
+    public static int CalculateSumOfSquares(int max) => Enumerable.Range(1, max).Sum(x => x * x);
 
-    public static int CalculateDifferenceOfSquares(int max)
-    {
-        throw new NotImplementedException("You need to implement this function.");
-    }
+    public static int CalculateDifferenceOfSquares(int max) => CalculateSquareOfSum(max) - CalculateSumOfSquares(max);
 }

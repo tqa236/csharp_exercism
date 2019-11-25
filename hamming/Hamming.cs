2b8b7ed4c3616130167b96b653e42c6421fef6f8
Quoc Anh Trinh <trinhquocanh94@gmail.com>
2019-11-25 16:16:38 +0100

    Solve Hamming

--- hamming/Hamming.cs
+++ hamming/Hamming.cs
@@ -4,6 +4,15 @@ public static class Hamming
 {
     public static int Distance(string firstStrand, string secondStrand)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        if (firstStrand.Length != secondStrand.Length)
+        {
+            throw new ArgumentException("The length of two strands should be the same.");
+        }
+        int count = 0;
+        for (int i = 0; i < firstStrand.Length; i++)
+        {
+            if (firstStrand[i] != secondStrand[i]) count = count + 1;
+        }
+        return count;
     }
 }

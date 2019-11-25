612001e3b27df707485dc7c29cb0e71fd33d74db
Quoc Anh Trinh <trinhquocanh94@gmail.com>
2019-11-25 15:23:30 +0100

    Solve High Score

--- high-scores/HighScores.cs
+++ high-scores/HighScores.cs
@@ -2,30 +2,15 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
-public class HighScores
-{
-    public HighScores(List<int> list)
-    {
-        throw new NotImplementedException();
-    }
+public class HighScores {
+    private List<int> list;
+    public HighScores (List<int> list) => this.list = list;
 
-    public List<int> Scores()
-    {
-        throw new NotImplementedException();
-    }
+    public List<int> Scores () => list;
 
-    public int Latest()
-    {
-        throw new NotImplementedException();
-    }
+    public int Latest () => list[list.Count - 1];
 
-    public int PersonalBest()
-    {
-        throw new NotImplementedException();
-    }
+    public int PersonalBest () => list.Max ();
 
-    public List<int> PersonalTopThree()
-    {
-        throw new NotImplementedException();
-    }
+    public List<int> PersonalTopThree () => list.OrderByDescending (r => r).Take (3).ToList ();
 }

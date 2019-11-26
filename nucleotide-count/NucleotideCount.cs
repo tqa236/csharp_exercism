5afbc3f0b662ff927cd768eb69c79243f3316a7d
Quoc Anh Trinh <trinhquocanh94@gmail.com>
2019-11-26 11:39:10 +0100

    Solve Nucleotide Count

--- nucleotide-count/NucleotideCount.cs
+++ nucleotide-count/NucleotideCount.cs
@@ -1,10 +1,17 @@
 using System;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Linq;
 
 public static class NucleotideCount
 {
     public static IDictionary<char, int> Count(string sequence)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        if (!Regex.IsMatch(sequence, @"^[ACGT]*$"))
+        {
+            throw new ArgumentException("Invalid strand.");
+        }
+        return (sequence + "ACGT").GroupBy(x => x).ToDictionary(g => g.Key, g => g.Count() - 1);
+
     }
 }
